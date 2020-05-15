ea6ddf023ee7ab9b17a39b40caf15f28bba37477
emil-international <58439947+emil-international@users.noreply.github.com>
2020-05-15 17:07:04 +0300

    adding papkas structure

--- Dev/Development/Main/Main.py
+++ Dev/Development/Main/Main.py
@@ -10,29 +10,29 @@ level_passed = 1
 
 class resourses:
     def __init__(self):
-        self.Zombie_usual_right = pyglet.image.load('zombie_right.png')
-        self.Zombie_usual_left = pyglet.image.load('zombie_left.png')
-        self.hero_right = pyglet.image.load('hero_right.png')
-        self.hero_left = pyglet.image.load('hero_left.png')
-        self.sniper_bullet = pyglet.image.load('bullet.png')
-        self.menu_level_1 = pyglet.image.load('icon_level_1.png')
-        self.menu_level_2 = pyglet.image.load('icon_level_2.png')
-        self.menu_level_3 = pyglet.image.load('icon_level_3.png')
-        self.menu_level_4 = pyglet.image.load('icon_level_4.png')
-        self.menu_level_5 = pyglet.image.load('icon_level_5.png')
-        self.phon_level_1 = pyglet.image.load('level_1_phon.bmp')
-        self.menu_map = pyglet.image.load('icon_map.png')
-        self.phon_success = pyglet.image.load('phon_success.png')
-        self.phon_fail = pyglet.image.load('fail.png')
-        self.phon_menu = pyglet.image.load('phon_menu.png')
+        self.Zombie_usual_right = pyglet.image.load('res/zombie_right.png')
+        self.Zombie_usual_left = pyglet.image.load('res/zombie_left.png')
+        self.hero_right = pyglet.image.load('res/hero_right.png')
+        self.hero_left = pyglet.image.load('res/hero_left.png')
+        self.sniper_bullet = pyglet.image.load('res/bullet.png')
+        self.menu_level_1 = pyglet.image.load('res/icon_level_1.png')
+        self.menu_level_2 = pyglet.image.load('res/icon_level_2.png')
+        self.menu_level_3 = pyglet.image.load('res/icon_level_3.png')
+        self.menu_level_4 = pyglet.image.load('res/icon_level_4.png')
+        self.menu_level_5 = pyglet.image.load('res/icon_level_5.png')
+        self.phon_level_1 = pyglet.image.load('res/level_1_phon.bmp')
+        self.menu_map = pyglet.image.load('res/icon_map.png')
+        self.phon_success = pyglet.image.load('res/phon_success.png')
+        self.phon_fail = pyglet.image.load('res/fail.png')
+        self.phon_menu = pyglet.image.load('res/phon_menu.png')
 
-        self.zombie_fast_left = pyglet.image.load('zombie_fast_left.png')
-        self.zombie_fast_right = pyglet.image.load('zombie_fast_right.png')
+        self.zombie_fast_left = pyglet.image.load('res/zombie_fast_left.png')
+        self.zombie_fast_right = pyglet.image.load('res/zombie_fast_right.png')
 
-        self.boss_left = pyglet.image.load('boss_left.png')
-        self.boss_right = pyglet.image.load('boss_right.png')
+        self.boss_left = pyglet.image.load('res/boss_left.png')
+        self.boss_right = pyglet.image.load('res/boss_right.png')
 
-        self.cloning = pyglet.image.load('cloning.png')
+        self.cloning = pyglet.image.load('res/cloning.png')
 
 
 class Interface_elements:
